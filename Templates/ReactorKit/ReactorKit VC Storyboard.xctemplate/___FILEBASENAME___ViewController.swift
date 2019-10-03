bf760b947fb28765062fcf2e35b9950831200862
Kanz <87kangsw@gmail.com>
2019-10-03 20:37:32 +0900

    # Storyboard 기반 ReactorKit ViewController & ViewReactor 템플릿 추가

---
+++ Templates/ReactorKit/ReactorKit VC Storyboard.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,39 @@
+//___FILEHEADER___
+
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxDataSources
+import RxSwift
+
+class ___FILEBASENAMEASIDENTIFIER___: BaseViewController, StoryboardView, ReactorBased {
+
+    typealias Reactor = <#Reactor#>
+    
+    // MARK: - UI
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - UI Setup
+    private func configureUI() {
+        
+    }
+    
+    // MARK: - Binding
+    func bind(reactor: Reactor) {
+        
+        // Action
+        
+        // State
+    }
+    
+    // MARK: - Route
+}
---
+++ Templates/ReactorKit/ReactorKit VC Storyboard.xctemplate/___FILEBASENAME___ViewReactor.swift
@@ -0,0 +1,41 @@
+//___FILEHEADER___
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+
+class ___FILEBASENAMEASIDENTIFIER___: Reactor {
+    
+    enum Action {
+        // case <#case#>
+    }
+    
+    enum Mutation {
+        // case <#case#>
+    }
+    
+    struct State {
+        // <#fields#>
+    }
+    
+    let initialState = State()
+    
+    // MARK: Mutation
+    func mutate(action: Action) -> Observable<Mutation> {
+//        switch <#value#> {
+//        case <#pattern#>:
+//            <#code#>
+//        }
+        return .empty()
+    }
+    
+    // MARK: Reduce
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+//        switch mutation {
+//        case <#pattern#>:
+//            <#code#>
+//        }
+        return state
+    }
+}
